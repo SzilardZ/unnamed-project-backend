52f8a22df5e93338053d89d6a8d159b0e9c59e81
SzilardZ <szilard.zr@gmail.com>
2019-03-23 22:37:00 +0100

    Initial commit: base spring boot application file structure

---
+++ src/main/java/com/szilardz/mediarectification/MediaRectificationApplication.java
@@ -0,0 +1,13 @@
+package com.szilardz.mediarectification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaRectificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaRectificationApplication.class, args);
+    }
+
+}
